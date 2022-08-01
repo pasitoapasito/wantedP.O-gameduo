382302b78e198a3236dfff0e5915398acba0f715
DGK <pasito.dev@gmail.com>
2022-08-02 08:04:24 +0900

    feat: 유저 로그아웃 기능 구현 (#4)

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from users.views.user_signup import UserSignUpView
-from users.views.user_signin import UserSignInView
+from users.views.user_signup  import UserSignUpView
+from users.views.user_signin  import UserSignInView
+from users.views.user_signout import UserSignOutView
 
 
 urlpatterns = [
     path('/signup', UserSignUpView.as_view()),
     path('/signin', UserSignInView.as_view()),
+    path('/signout', UserSignOutView.as_view()),
 ]
