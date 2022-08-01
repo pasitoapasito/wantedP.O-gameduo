cd640cad48112624182f2e735f9d3af03664fdaa
DGK <pasito.dev@gmail.com>
2022-08-01 14:45:06 +0900

    feat: 프로젝트 초기세팅 (#1)

---
+++ gameduo/urls.py
@@ -0,0 +1,34 @@
+from drf_yasg       import openapi
+from drf_yasg.views import get_schema_view
+
+from rest_framework import permissions
+from django.urls    import path, re_path, include
+
+"""
+Swagger settings
+"""
+schema_view = get_schema_view(
+    openapi.Info(
+        title='gameduo API',
+        default_version='v1',
+        description='gameduo-project',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+"""
+Django app url patterns
+"""
+urlpatterns = [
+
+]
+
+"""
+Swagger url patterns
+"""
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',schema_view.without_ui(cache_timeout=0),name='schema-json',),
+    re_path(r'^swagger$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
