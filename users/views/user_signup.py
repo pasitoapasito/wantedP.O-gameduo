c242c000835bc744159b0bfb241f886c53f1c3df
DGK <pasito.dev@gmail.com>
2022-08-03 07:04:57 +0900

    add: 유저 회원가입, 로그인, 로그아웃, 토큰 재발급 기능에 주석설명 추가 (#5)

--- users/views/user_signup.py
+++ users/views/user_signup.py
@@ -8,11 +8,21 @@ from users.serializers import UserSignUpSerializer
 
 
 class UserSignUpView(APIView):
+    """
+    Assignee: 김동규
+    
+    request body: email, nickname, password
+    return: json
+    detail: 유저 회원가입 기능입니다.
+    """
     
     permission_classes = [AllowAny]
     
     @swagger_auto_schema(request_body=UserSignUpSerializer, responses={201: UserSignUpSerializer})
     def post(self, request):
+        """
+        POST: 유저 회원가입 기능
+        """
         serializer = UserSignUpSerializer(data=request.data)
         
         if serializer.is_valid():
