6be13d7d535cbda07cd947778db1d86642ba9d83
DGK <pasito.dev@gmail.com>
2022-08-04 20:03:07 +0900

    feat: 보스레이드 종료 기능구현 (#10)

--- raids/urls.py
+++ raids/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from raids.views.raid_enter import BossRaidEnterView
+from raids.views.raid_end   import BossRaidEndView
 
 
 urlpatterns = [
     path('/enter', BossRaidEnterView.as_view()),
+    path('/<int:raid_history_id>/end', BossRaidEndView.as_view()),
 ]
